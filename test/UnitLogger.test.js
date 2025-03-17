d0eab996c0f8dd243d0224c8f6ca93408d4e89c0
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-03-17 16:07:41 +0100

    Added getFileFormat unit test

--- test/UnitLogger.test.js
+++ test/UnitLogger.test.js
@@ -1,6 +1,8 @@
 const formats = require('../Config/Loggers/Formats');
 const assert = require("assert");
 const sinon = require("sinon");
+const rewire = require("rewire");
+const winston = require("winston");
 
 
 afterEach(() => {
@@ -24,10 +26,38 @@ describe('Unit get Formats', function () {
 
     it('select error base format', function () {
         const format = formats.selectBaseFormat("error");
-        assert(format.template, formats.getErrorFormat)    
+        assert.equal(format.template, formats.getErrorFormat)    
     });
     it('select info base format', function () {
         const format = formats.selectBaseFormat("info");
-        assert(format.template, formats.getInfoFormat)    
+        assert.equal(format.template, formats.getInfoFormat)    
+    });
+    it('getFileFormat', function () {
+        var FormatsMod = rewire("../Config/Loggers/Formats.js");
+
+        function mocked_combine () {
+            assert.equal(arguments.length, 3)
+            assert.deepEqual(arguments[0].options.label, "A")
+            assert.equal(Object.keys(arguments[1].options).length, 0)
+            assert.equal(arguments[2].template.name, formats.getInfoFormat.name)
+            assert.equal(typeof arguments[2], typeof winston.format.printf())
+        };
+        FormatsMod.__set__("combine", mocked_combine);
+        FormatsMod.getFileFormat("A", "info");
+    });
+
+    it('getConsoleFormat', function () {
+        var FormatsMod = rewire("../Config/Loggers/Formats.js");
+
+        function mocked_combine () {
+            assert.equal(arguments.length, 4)
+            assert.deepEqual(arguments[0].options.label, "B")
+            assert.equal(Object.keys(arguments[1].options).length, 0)
+            assert.equal(arguments[2].options.colors.error, "red")
+            assert.equal(arguments[3].template.name, formats.getErrorFormat.name)
+            assert.equal(typeof arguments[3], typeof winston.format.printf())
+        };
+        FormatsMod.__set__("combine", mocked_combine);
+        FormatsMod.getConsoleFormat("B", "error");
     });
 });
