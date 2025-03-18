3571ae9aafc23ea6cbae8045458a15d4f09c31e0
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-03-18 08:31:49 +0100

    Refactor, use child in main runner

--- Runner.js
+++ Runner.js
@@ -1,10 +1,5 @@
 const logger = require("./Config/Loggers/LoggerFactory").createDefaultLogger(__filename)
 
 logger.log("info", "Running main...")
-
-const preconfig = require("./Runners/PreConfig")
-
+const preconfig = require("./Runners/ChildRunner")
 logger.log("error", "Error in main")
-
-const postconfig = require("./Runners/PostConfig")
-
