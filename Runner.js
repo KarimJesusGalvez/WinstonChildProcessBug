18f7c4b9e6368b75ede686eca2b9be994a20f1c6
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-03-18 15:24:20 +0100

    Refactor, stdin in child runners

--- Runner.js
+++ Runner.js
@@ -1,5 +1,5 @@
 const logger = require("./Config/Loggers/LoggerFactory").createDefaultLogger(__filename)
 
 logger.log("info", "Running main...")
-const preconfig = require("./Runners/ChildRunner")
+require("./Runners/ChildRunner")
 logger.log("error", "Error in main")
--- Runners/PreConfig.js
+++ Runners/PreConfig.js
@@ -1,4 +1,6 @@
 const logger = require("../Config/Loggers/LoggerFactory").createDefaultLogger(__filename)
 
 logger.log("info", "Running Preconfig...")
+console.info("Preconfig stdin")
+console.error("Preconfig stderr")
 logger.log("error", "Error in PreConfig")
