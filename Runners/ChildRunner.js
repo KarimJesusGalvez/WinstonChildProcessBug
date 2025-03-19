d321d7bbd49f7c082636f3021f69a6b473d82d19
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-03-19 12:12:57 +0100

    Refactor, child process debug stm

--- Runners/ChildRunner.js
+++ Runners/ChildRunner.js
@@ -1,6 +1,7 @@
 "use strict"
 const logger = require("../Config/Loggers/LoggerFactory").createDefaultLogger(__filename)
 const {execSync, exec} = require("node:child_process")
+const {pid, title} = require("node:process")
 
 function exec_cmd (cmd, sync = true) {
     logger.log("info", "Starting " + (sync ? 'sync ': 'async ') + "task: " + cmd);
@@ -8,9 +9,9 @@ function exec_cmd (cmd, sync = true) {
         if (sync) {return execSync( cmd, { stdio: 'inherit' } )}
         else {
             let child = exec(cmd, { stdio: 'inherit' });
-            child.on('spawn', ()=> logger.log("info", 'async ' + "task: " + cmd  +" Started"));
+            child.on('spawn', ()=> logger.log("debug", "async task: '".concat(cmd, "' Started from main process ", pid, ": ", title)));
             child.stdout.on('data', parseChildData);
-            child.on('close', ()=> logger.log("info", 'async ' + "task: '" + cmd  +"' finished with code " + child.exitCode));
+            child.on('close', ()=> logger.log("info", "async task: '".concat(cmd,"' finished with code ",child.exitCode)));
         };
     }
     catch (error){ console.error("Error in exec_cmd: " + error) };
